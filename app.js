fdc663a2be4525bfdcc58496d61c05ba6c2a6ce1
la_victoire <oghuvbuvictory@gmail.com>
2025-03-20 03:39:40 +0100

    modified:   app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-app.use(cors({origin:"https://kuku-api-production.up.railway.app",credentials: true,}));
+app.use(cors({origin:"http://localhost:3000",credentials: true,}));
 app.use(session({
   secret: JWT_SECRET,
   resave:false,
