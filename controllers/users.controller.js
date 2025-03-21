be6d075ec4b3aecb8f204fdfbe629e4304095f32
la_victoire <oghuvbuvictory@gmail.com>
2025-03-21 09:19:07 +0100

    modified:   controllers/users.controller.js

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -2,13 +2,15 @@ import { ADMIN_ID } from "../config/env.js";
 import User from "../models/users.model.js"
 
 export const getUserById = async (req, res, next) => {
+  const user = await User.findById(req.params.id);
   try {
-    const user = await User.findById(req.params._id);
-  
+   console.log(user);
+   console.log(req.params.id);
+   
     if (!user) {
-      res.status(404).json({message:"User Not Found !!!"})
+      return res.status(404).json({message:"User Not Found !!!"})
     }
-    res.status(200).json({success:true,data:user})
+    return res.status(200).json({success:true,data:user})
     
   } catch (error) {
     next(error)
